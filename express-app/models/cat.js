0095d86622005eafe04cdc7adc8309b0550a5b85
Anvido <adgonzalezo@unal.edu.co>
2019-09-20 20:27:50 -0500

    delte trash

--- express-app/models/cat.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('../db')
-const Cat = mongoose.model('Cat', { name: String })
-
-//const kitty = new Cat({ name: 'Ishkgur' })
-//kitty.save().then(() => console.log('meow'))
-
-module.exports =  Cat
